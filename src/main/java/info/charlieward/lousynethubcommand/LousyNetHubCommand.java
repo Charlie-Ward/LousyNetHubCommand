ee7318d06df64632d5c549a5b1a9ef11b6b1cae1
Charlie Ward <59178341+Charlie-Ward@users.noreply.github.com>
2024-02-25 22:23:33 +0000

    Got it working

--- src/main/java/info/charlieward/lousynethubcommand/LousyNetHubCommand.java
+++ src/main/java/info/charlieward/lousynethubcommand/LousyNetHubCommand.java
@@ -1,17 +1,52 @@
 package info.charlieward.lousynethubcommand;
 
+import info.charlieward.lousynethubcommand.commands.hubCommand;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+import redis.clients.jedis.Jedis;
+
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
 
 public final class LousyNetHubCommand extends JavaPlugin {
 
+    private static LousyNetHubCommand plugin;
+    public Jedis jedis = new Jedis();
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
 
+        plugin = this;
+        this.getServer().getMessenger().registerOutgoingPluginChannel(this, "BungeeCord");
+
+        getLogger().info("LousyNet-HubCommand v." + this.getDescription().getVersion() + " has loaded.");
+
+        getCommand("hub").setExecutor(new hubCommand(this));
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        getLogger().info("LousyNet-HubCommands v." + this.getDescription().getVersion() + " has been disabled.");
+    }
+
+    public static LousyNetHubCommand getPlugin() {
+        return plugin;
+    }
+
+    public static void sendPlayerToServer(Player player, String server) {
+        try {
+            ByteArrayOutputStream b = new ByteArrayOutputStream();
+            DataOutputStream out = new DataOutputStream(b);
+            out.writeUTF("Connect");
+            out.writeUTF(server);
+            player.sendPluginMessage(LousyNetHubCommand.getPlugin(), "BungeeCord", b.toByteArray());
+            b.close();
+            out.close();
+        }
+        catch (Exception e) {
+            player.sendMessage(ChatColor.RED+"Error when trying to connect to "+server);
+            System.out.println(e);
+        }
     }
 }
--- src/main/java/info/charlieward/lousynethubcommand/commands/hubCommand.java
+++ src/main/java/info/charlieward/lousynethubcommand/commands/hubCommand.java
@@ -1,2 +1,47 @@
-package info.charlieward.lousynethubcommand.commands;public class hubCommands {
+package info.charlieward.lousynethubcommand.commands;
+
+import info.charlieward.lousynethubcommand.LousyNetHubCommand;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class hubCommand implements CommandExecutor {
+
+    static LousyNetHubCommand plugin;
+
+    public hubCommand(LousyNetHubCommand plugin){
+        this.plugin = plugin;
+    }
+    @Override
+    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+        if (commandSender instanceof Player) {
+            Player player = (Player) commandSender;
+            String serverStatus = plugin.jedis.get("mainHub");
+            if (serverStatus == null || serverStatus.equals("offline")) {
+                player.sendMessage(ChatColor.BLUE + "[LousyNet] " + ChatColor.WHITE + "The hub server is not currently online");
+            } else {
+                player.sendMessage(ChatColor.BLUE + "[LousyNet] " + ChatColor.WHITE + "Sending you to hub");
+                LousyNetHubCommand.sendPlayerToServer(player, "hub");
+            }
+        }
+        return true;
+    }
+
+    public void itemClicked(String serverID, String serverName, Player player) {
+        String serverStatus = plugin.jedis.get(serverID);
+        if (serverStatus == null || serverStatus.equals("offline")) {
+            player.sendMessage(ChatColor.BLUE + "[LousyNet] " + ChatColor.WHITE + "This server is not currently online");
+        } else {
+            if (serverID == "") {
+                player.sendMessage(ChatColor.BLUE + "[LousyNet] " + ChatColor.WHITE + "This server is not currently online");
+            } else if (serverID == "mainHub") {
+                player.sendMessage(ChatColor.BLUE + "[LousyNet] " + ChatColor.WHITE + "You are already connected to this server");
+            } else {
+                player.sendMessage(ChatColor.BLUE + "[LousyNet] " + ChatColor.WHITE + "Sending you to " + serverName);
+                LousyNetHubCommand.sendPlayerToServer(player, serverID);
+            }
+        }
+    }
 }
